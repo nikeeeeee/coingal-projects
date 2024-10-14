61e89c11f9db5dc0041b2d9338257d37c26f6384
nikeeeeee <fae908060@gmail.com>
2024-10-14 18:48:03 +0100

    initial commit

---
+++ pass.py
@@ -0,0 +1,6 @@
+for x in range (10): #iterate for loop
+    if x % 20 == 0: #condition 1
+        print("twist")
+    elif x % 15 == 0: #condition 2
+        pass      #pass statement
+    elif x % 5 == 0: #condition
