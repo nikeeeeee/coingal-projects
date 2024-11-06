61c9d02c131229b71a4b0004bfe6f7081b9da6c1
nikeeeeee <fae908060@gmail.com>
2024-11-06 18:59:20 +0100

    initial commit

---
+++ operations on ssets.py
@@ -0,0 +1,13 @@
+# different types of sets in python
+# set of integers
+my_set = {1, 2, 3}
+print(my_set)
+# set of mixed datatypes
+my_set = {1.0, "hello",(1, 2, 3)}
+print(my_set)
+# set cannot have duplicates
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+# we can make set from a list
+my_set = set{[1,2,3,2]}
+print(my_set)
