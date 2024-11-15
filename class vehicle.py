b88626aeb74b13dd5dabb0a446deff1d6d210fff
nikeeeeee <fae908060@gmail.com>
2024-11-15 19:41:40 +0100

    initial commit

---
+++ class vehicle.py
@@ -0,0 +1,8 @@
+# create class
+class vehicle:
+    # create init method
+    def _init_(self, max_speed, mileage):
+                # bind the arguments
+        self.max_speed = max_speed
+        self.mileage = mileage 
+    # object creation
