08f6a11fbc746dbcc61c6f52528d824a317d05e4
nikeeeeee <fae908060@gmail.com>
2024-11-20 18:29:03 +0100

    initial commit

---
+++ string upper case.py
@@ -0,0 +1,18 @@
+#  create class
+class IOString():
+        # constructor to set default value
+    def _init_(self):
+       self.strl = ""
+    # function to get input from user 
+    def get_string(self):
+        self.str1 = input("enter string : ")
+        # function to put the string in upper case
+    def print_string(self):
+        print("result is :", self.str1.upper())
+# object creation
+str1 = IOString()
+# call functions
+str1.get_string()
+str1.print_print_string()
+
+    
